af459067ecd26db4e0c09a00b9d5dbb69211d31b
DouglasUtf <133456856+DouglasUtf@users.noreply.github.com>
2023-06-16 23:56:23 +0000

    Change Inicial Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h2 className='text-center mt-16 bg-neutral-500'> Home from Douglas</h2>
+    <h2 className='text-center mt-16 bg-red-300'>Home from Douglas</h2>
   )
 }
 
