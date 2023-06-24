6aa68a62fc9882abb1c52682a60f8857c43412b8
DouglasUtf <133456856+DouglasUtf@users.noreply.github.com>
2023-06-24 01:21:26 +0000

    Dashboard ATT2

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,9 +7,9 @@ import { Client } from '@vercel/postgres'
 
 
 const navigation = [
-  { name: 'Home', href: '#' },
-  { name: 'Cadastrar Cursos', href: '#' },
-  { name: 'Listar cursos', href: '#' },
+  { name: 'Home', href: '/admin/' },
+  { name: 'Cadastrar Cursos', href: '/admin/course/new' },
+  { name: 'Listar cursos', href: '/admin/course' },
   
 ]
 
@@ -107,7 +107,7 @@ export default function Admin() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#a71b2d] to-[#ff0909] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -119,6 +119,7 @@ export default function Admin() {
 
           </div>
           <div className="text-center">
+
             <h1 className="text-4xl mt-10 font-bold tracking-tight text-gray-900 sm:text-6xl">
               Bem Vindo Admin!! 
             </h1>
